0ee9da860ac52031f808f17ff7232d0ca9e3c02f
skipp <chanchanakoedtho@gmail.com>
2022-03-31 12:54:18 +0700

    Add support swift package

---
+++ Tests/RxGoogleMapsTests/RxGoogleMapsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RxGoogleMaps
+
+final class RxGoogleMapsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RxGoogleMaps().text, "Hello, World!")
+    }
+}
